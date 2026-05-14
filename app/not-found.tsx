80795f2e52c2948f41eb71dfbf437cd152a2dbeb
liagic <leon_max@i.ua>
2026-05-14 20:01:51 +0300

    Removed use route

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from 'next';
 import css from '@/app/Home.module.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Not found',
@@ -23,19 +21,13 @@ export const metadata: Metadata = {
 };
 
 const NotFound = () => {
-  const router = useRouter();
-  useEffect(() => {
-    const timer = setTimeout(() => router.push('/'), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
-    <>
+    <div>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
-    </>
+    </div>
   );
 };
 export default NotFound;
